424119d63e9cfbf61a1ff0520a677e73f953148f
fathurramadhanfahmi <146045262+fathurramadhanfahmi@users.noreply.github.com>
2024-10-06 15:30:00 +0700

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,13 @@
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('./sw.js')
+        .then(registration => {
+            console.log('Service Worker registered with scope:', registration.scope);
+        })
+        .catch(error => {
+            console.error('Service Worker registration failed:', error);
+        });
+    });
+} else {
+    console.warn('Service Workers are not supported in this browser.');
+}
---
+++ sw.js
@@ -0,0 +1,54 @@
+const cache_name = 'v1';
+const cache_assets = [
+    'index.html',
+    'contact.html',
+    'about.html',
+    'offline.html',
+    './apps.js',
+    './style.css'
+];
+
+// Install event
+self.addEventListener('install', (event) => {
+    console.log('Service Worker: Installed');
+    event.waitUntil(
+        caches.open(cache_name).then((cache) => {
+            console.log('Service Worker: Caching Files');
+            return cache.addAll(cache_assets); // Tambahkan "return" untuk memastikan proses selesai
+        })
+    );
+    self.skipWaiting(); // Memastikan service worker segera aktif
+});
+
+// Activate event
+self.addEventListener('activate', (event) => {
+    console.log('Service Worker: Activated');
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== cache_name) {
+                        console.log('Service Worker: Clearing Old Cache');
+                        return caches.delete(cache); // Hapus cache lama
+                    }
+                })
+            );
+        })
+    );
+    self.clients.claim(); // Mengambil kontrol dari service worker lama
+});
+
+// Fetch event
+self.addEventListener('fetch', (event) => {
+    console.log('Service Worker: Fetching');
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            // Jika ada cache, kembalikan respon dari cache
+            if (response) {
+                return response;
+            }
+            // Jika tidak ada cache, lakukan fetch request
+            return fetch(event.request).catch(() => caches.match('offline.html')); // Tampilkan halaman offline jika fetch gagal
+        })
+    );
+});
